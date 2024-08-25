25f02060af679c925f2a07f8add823d91ce4d34e
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-08-26 00:11:22 +1000

    Build the hoot and coment models

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const cors = require('cors');
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const testJWTRouter = require('./controllers/test-jwt');
-const usersRouter = require('./controllers/users');
-const profilesRouter = require('./controllers/profiles');
+const cors = require("cors");
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+const testJWTRouter = require("./controllers/test-jwt");
+const usersRouter = require("./controllers/users");
+const profilesRouter = require("./controllers/profiles");
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-app.use(cors());
-app.use(express.json());
+server.use(cors());
+server.use(express.json());
 
 // Routes go here
-app.use('/test-jwt', testJWTRouter);
-app.use('/users', usersRouter);
-app.use('/profiles', profilesRouter);
+server.use("/test-jwt", testJWTRouter);
+server.use("/users", usersRouter);
+server.use("/profiles", profilesRouter);
 
-app.listen(3000, () => {
-    console.log('The express app is ready!');
+server.listen(3005, () => {
+  console.log("The express server is ready!");
 });
